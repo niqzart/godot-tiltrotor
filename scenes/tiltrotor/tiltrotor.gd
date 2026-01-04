279126c6e2d3cc64dce323fad56ff6cacbdba37c
niqzart <qwert45hi@yandex.ru>
2026-01-04 18:18:31 +0300

    feat: rolling crashtest

--- scenes/tiltrotor/tiltrotor.gd
+++ scenes/tiltrotor/tiltrotor.gd
@@ -1,9 +1,16 @@
 extends RigidBody3D
 
 
+var is_first = true
+
+
 func _physics_process(_delta: float) -> void:
     var right_force: Vector3 = self.transform.basis.y * 5
     var left_force: Vector3 = self.transform.basis.y * 5
+
+    if is_first:
+        right_force *= 4
+        is_first = false
 
     self.apply_force(right_force, self.transform.basis.x * 2)
     self.apply_force(left_force, self.transform.basis.x * -2)
