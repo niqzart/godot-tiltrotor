c91a06babddd207ce54b38322ce427b16beaa4f5
niqzart <qwert45hi@yandex.ru>
2026-01-04 18:24:59 +0300

    feat: per-rotor power controls (for rolling)

--- scenes/tiltrotor/tiltrotor.gd
+++ scenes/tiltrotor/tiltrotor.gd
@@ -1,16 +1,23 @@
 extends RigidBody3D
 
 
-var is_first = true
-
-
 func _physics_process(_delta: float) -> void:
-    var right_force: Vector3 = self.transform.basis.y * 5
-    var left_force: Vector3 = self.transform.basis.y * 5
+    var right_force: Vector3 = self.transform.basis.y
+    var left_force: Vector3 = self.transform.basis.y
 
-    if is_first:
-        right_force *= 4
-        is_first = false
+    if Input.is_action_pressed("right_rotor_up"):
+        right_force *= 10
+    elif Input.is_action_pressed("right_rotor_down"):
+        right_force *= -10
+    else:
+        right_force *= 5
+
+    if Input.is_action_pressed("left_rotor_up"):
+        left_force *= 10
+    elif Input.is_action_pressed("left_rotor_down"):
+        left_force *= -10
+    else:
+        left_force *= 5
 
     self.apply_force(right_force, self.transform.basis.x * 2)
     self.apply_force(left_force, self.transform.basis.x * -2)
