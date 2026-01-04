3ea28ca9b4038c2dc4ae82f017eefa43df0e9ecc
niqzart <qwert45hi@yandex.ru>
2026-01-04 18:24:18 +0300

    feat: tilting the rotors for yaw & pitch

--- scenes/tiltrotor/tiltrotor.gd
+++ scenes/tiltrotor/tiltrotor.gd
@@ -2,8 +2,25 @@ extends RigidBody3D
 
 
 func _physics_process(_delta: float) -> void:
+    var right_position: Vector3 = self.transform.basis.x * 2
+    var left_position: Vector3 = self.transform.basis.x * -2
+
     var right_force: Vector3 = self.transform.basis.y
     var left_force: Vector3 = self.transform.basis.y
+
+    if Input.is_action_pressed("right_rotor_forwards"):
+        right_position += self.transform.basis.z
+        right_force -= self.transform.basis.z
+    elif Input.is_action_pressed("right_rotor_backwards"):
+        right_position -= self.transform.basis.z
+        right_force += self.transform.basis.z
+
+    if Input.is_action_pressed("left_rotor_forwards"):
+        left_position += self.transform.basis.z
+        left_force -= self.transform.basis.z
+    elif Input.is_action_pressed("left_rotor_backwards"):
+        left_position -= self.transform.basis.z
+        left_force += self.transform.basis.z
 
     if Input.is_action_pressed("right_rotor_up"):
         right_force *= 10
@@ -19,5 +36,5 @@ func _physics_process(_delta: float) -> void:
     else:
         left_force *= 5
 
-    self.apply_force(right_force, self.transform.basis.x * 2)
-    self.apply_force(left_force, self.transform.basis.x * -2)
+    self.apply_force(right_force, right_position)
+    self.apply_force(left_force, left_position)
