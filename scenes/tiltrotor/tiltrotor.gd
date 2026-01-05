c2988b6d325b5115eb4da68076588d715076f365
niqzart <qwert45hi@yandex.ru>
2026-01-05 07:41:19 +0300

    feat: second version of controls with lateral movement

--- scenes/tiltrotor/tiltrotor.gd
+++ scenes/tiltrotor/tiltrotor.gd
@@ -54,9 +54,16 @@ func _process(_delta: float) -> void:
         self._update_rotor_color()
 
 
-func _physics_process(delta: float) -> void:
-    print(self.global_position, self.global_rotation)
+enum InputMode {
+    COPLEX_V1,
+    COPLEX_V2,
+}
 
+
+var hover_input_mode: InputMode = InputMode.COPLEX_V2
+
+
+func _apply_forces_from_inputs_complex_v1() -> void:
     var right_position: Vector3 = self.transform.basis.x * 2
     var left_position: Vector3 = self.transform.basis.x * -2
 
@@ -93,6 +100,64 @@ func _physics_process(delta: float) -> void:
 
     self.apply_force(right_force, right_position)
     self.apply_force(left_force, left_position)
+
+
+func _apply_forces_from_inputs_complex_v2() -> void:
+    var right_power: float
+    var left_power: float
+
+    if Input.is_action_pressed("right_rotor_collective_up"):
+        right_power = 10
+    elif Input.is_action_pressed("right_rotor_collective_down"):
+        right_power = -10
+    else:
+        right_power = 5
+
+    if Input.is_action_pressed("left_rotor_collective_up"):
+        left_power = 10
+    elif Input.is_action_pressed("left_rotor_collective_down"):
+        left_power = -10
+    else:
+        left_power = 5
+
+    var right_force_direction: Vector3 = self.transform.basis.y
+    var left_force_direction: Vector3 = self.transform.basis.y
+
+    if Input.is_action_pressed("right_rotor_collective_forwards"):
+        right_force_direction -= self.transform.basis.z
+    elif Input.is_action_pressed("right_rotor_collective_backwards"):
+        right_force_direction += self.transform.basis.z
+
+    if Input.is_action_pressed("left_rotor_collective_forwards"):
+        left_force_direction -= self.transform.basis.z
+    elif Input.is_action_pressed("left_rotor_collective_backwards"):
+        left_force_direction += self.transform.basis.z
+
+    var right_position: Vector3 = self.transform.basis.x * 2
+    var left_position: Vector3 = self.transform.basis.x * -2
+
+    if Input.is_action_pressed("right_rotor_cyclic_forwards"):
+        right_position += self.transform.basis.z
+    elif Input.is_action_pressed("right_rotor_cyclic_backwards"):
+        right_position -= self.transform.basis.z
+
+    if Input.is_action_pressed("left_rotor_cyclic_forwards"):
+        left_position += self.transform.basis.z
+    elif Input.is_action_pressed("left_rotor_cyclic_backwards"):
+        left_position -= self.transform.basis.z
+
+    self.apply_force(right_force_direction * right_power, right_position)
+    self.apply_force(left_force_direction * left_power, left_position)
+
+
+func _physics_process(delta: float) -> void:
+    print(self.global_position, self.global_rotation)
+
+    match self.hover_input_mode:
+        InputMode.COPLEX_V1:
+            self._apply_forces_from_inputs_complex_v1()
+        InputMode.COPLEX_V2:
+            self._apply_forces_from_inputs_complex_v2()
 
     if self.pid_enabled:
         # if Input.is_anything_pressed():  # TODO: do this once
