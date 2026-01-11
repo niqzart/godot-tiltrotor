26f1fc5301db989096c7084d532f7a1cb102dac3
niqzart <qwert45hi@yandex.ru>
2026-01-12 02:50:39 +0300

    feat: modular propeller (hitbox test)

---
+++ scenes/aircraft/components/propeller/propeller.gd
@@ -0,0 +1,25 @@
+extends RigidBody3D
+
+
+var rigid_body: RigidBody3D
+
+
+func _attach_to_parent() -> void:
+    var parent = self.get_parent()
+    if not is_instance_of(parent, RigidBody3D):
+        push_warning("Invalid parent, skipping attachment process")
+        self.rigid_body = self
+        return
+
+    self.rigid_body = parent
+
+    for child_node in self.get_children():
+        child_node.call_deferred("reparent", self.rigid_body)
+
+    self.freeze = true
+
+    # TODO: move parent's center of gravity & update the mass based on the rigid body
+
+
+func _ready() -> void:
+    self._attach_to_parent()
